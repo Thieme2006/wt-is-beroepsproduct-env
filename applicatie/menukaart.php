e9c02842ddb9e5d20220fc4c7bf780d4d5e21b8b
Thieme2006 <t.wijgman1@student.han.nl>
2023-12-06 08:52:34 +0100

    Menukaart

---
+++ applicatie/menukaart.php
@@ -0,0 +1,18 @@
+<?php
+
+$menu = array(
+    'Eten' ["Burgers" => 5.50, "Patat" => 6.34];
+    'Drinken' ["Cola" => 7.50]
+)
+
+foreach($menu as $menu_item => Bron)
+{
+echo 1menu_item
+foreach($row as $naam => $prijs)
+{
+    echo $naam " " $prijs
+}
+}
+
+?>
+
